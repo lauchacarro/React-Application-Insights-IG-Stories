d2ad5b76a212a2f41b31f4b1effa892fb9a3738e
Lautaro Carro <lcarro@algeiba.com>
2022-09-04 07:34:02 -0300

    agrego react-insta-stories

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,160 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Stories from "react-insta-stories";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="stories">
+        <Stories
+          loop
+          keyboardNavigation
+          defaultInterval={8000}
+          stories={stories2}
+          onStoryEnd={(s: any, st: any) => console.log("story ended", s, st)}
+          onAllStoriesEnd={(s: any, st: any) =>
+            console.log("all stories ended", s, st)
+          }
+          onStoryStart={(s: any, st: any) =>
+            console.log("story started", s, st)
+          }
+          storyContainerStyles={{ borderRadius: 8, overflow: "hidden" }}
+        />
+      </div>
     </div>
   );
 }
+
+const Story2 = ({ action, isPaused }: any) => {
+  return (
+    <div style={{ ...contentStyle, background: "Aquamarine", color: "#333" }}>
+      <h1>You get the control of the story.</h1>
+      <p>
+        Render your custom JSX by passing just a{" "}
+        <code style={{ fontStyle: "italic" }}>content</code> property inside
+        your story object.
+      </p>
+      <p>
+        You get a <code style={{ fontStyle: "italic" }}>action</code> prop as an
+        input to your content function, that can be used to play or pause the
+        story.
+      </p>
+      <h1>{isPaused ? "Paused" : "Playing"}</h1>
+      <h4>v2 is out 🎉</h4>
+      <p>React Native version coming soon.</p>
+    </div>
+  );
+};
+
+const stories2 = [
+  {
+    content: ({ action, isPaused }: any) => {
+      return (
+        <div style={contentStyle}>
+          <h1>The new version is here.</h1>
+          <p>This is the new story.</p>
+          <p>Now render React components right into your stories.</p>
+          <p>Possibilities are endless, like here - here's a code block!</p>
+          <pre>
+            <code style={code}>console.log('Hello, world!')</code>
+          </pre>
+          <p>Or here, an image!</p>
+          <br />
+          <img
+            style={image}
+            src="https://images.unsplash.com/photo-1565506737357-af89222625ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+          ></img>
+          <h3>Perfect. But there's more! →</h3>
+        </div>
+      );
+    },
+  },
+  {
+    content: ({ action, story }: any) => {
+      return (
+          <div style={{ background: "snow", padding: 20, height: "100%" }}>
+            <h1 style={{ marginTop: "100%", marginBottom: 0 }}>🌝</h1>
+            <h1 style={{ marginTop: 5 }}>
+              We have our good old image and video stories, just the same.
+            </h1>
+          </div>
+      );
+    },
+    seeMoreCollapsed: ({ toggleMore, action }: any) => (
+      <p style={customSeeMore} onClick={() => toggleMore(true)}>
+        A custom See More message →
+      </p>
+    ),
+    seeMore: ({ close }: any) => (
+      <div
+        style={{
+          maxWidth: "100%",
+          height: "100%",
+          padding: 40,
+          background: "white",
+        }}
+      >
+        <h2>Just checking the see more feature.</h2>
+        <p style={{ textDecoration: "underline" }} onClick={close}>
+          Go on, close this popup.
+        </p>
+      </div>
+    ),
+    duration: 5000,
+  },
+  {
+    url: "https://picsum.photos/1080/1920",
+    seeMore: ({ close }: any) => (
+      <div
+        style={{
+          maxWidth: "100%",
+          height: "100%",
+          padding: 40,
+          background: "white",
+        }}
+      >
+        <h2>Just checking the see more feature.</h2>
+        <p style={{ textDecoration: "underline" }} onClick={close}>
+          Go on, close this popup.
+        </p>
+      </div>
+    ),
+  },
+  {
+    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    type: "video",
+  },
+  {
+    content: Story2,
+  },
+];
+
+const image = {
+  display: "block",
+  maxWidth: "100%",
+  borderRadius: 4,
+};
+
+const code = {
+  background: "#eee",
+  padding: "5px 10px",
+  borderRadius: "4px",
+  color: "#333",
+};
+
+const contentStyle: React.CSSProperties = {
+  background: "#333",
+  width: "100%",
+  padding: 20,
+  color: "white",
+  height: "100%",
+};
+
+const customSeeMore: React.CSSProperties = {
+  textAlign: "center",
+  fontSize: 14,
+  bottom: 20,
+  position: "relative",
+};
 
 export default App;
